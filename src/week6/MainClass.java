6173481f545293381d73e527e37aaf34587c3ef4
uone0628 <121482693+uone0628@users.noreply.github.com>
2024-04-13 18:15:53 +0900

    dase code

---
+++ src/week6/MainClass.java
@@ -0,0 +1,12 @@
+package week6;
+
+import java.util.ArrayList;
+
+public class MainClass {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("World");
+        System.out.println(list);
+    }
+}
